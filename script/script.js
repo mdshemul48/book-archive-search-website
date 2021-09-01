2a1dfefa43739c0cd570f7047b5fc1cb15990ece
MD Shimul <mdshemul480@gmail.com>
2021-09-02 01:48:59 +0600

    adding card and showing post

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ const searchBox = elementById("search-input-box");
 const searchButton = elementById("search-input-button");
 
 const messageBox = elementById("messages");
+const resultArea = elementById("result_items");
 
 const showMessage = (message, alertType = "success") => {
   messageBox.innerHTML = `
@@ -13,12 +14,60 @@ const showMessage = (message, alertType = "success") => {
     `;
 };
 
+const createBookCard = (bookInfo) => {
+  const { title, author_name, first_publish_year, publisher, cover_i } =
+    bookInfo;
+
+  return `
+    <div>
+        <div class="card">
+            <img src="${
+              cover_i
+                ? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`
+                : "images/notFound.png"
+            }" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">${
+                  title ? title : "title not found."
+                }</h4>
+                <hr/>
+                ${
+                  author_name?.length > 0
+                    ? `<h6>By ${author_name.join(", ")}.</h6>`
+                    : "<h6>author not found</h6>"
+                }
+                <hr/>
+                ${
+                  publisher?.length > 0
+                    ? `<p class="my-0">Published by ${publisher.join(
+                        ", "
+                      )}.</p>`
+                    : "publisher not found"
+                }
+                <hr/>
+                <p>Fist released on ${first_publish_year}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
 const showResults = (bookResults) => {
   // if numFound === 0  thats mean no book found.. bookResults.numFound == 0 means false
   // if i reverse it !false === true.. then i will show not found message..
   if (!bookResults.numFound) {
     showMessage("Sorry, No Result Found :(", "warning");
+    return;
   }
+
+  showMessage(`About ${bookResults.numFound} results found.`);
+
+  const booksInfo = bookResults.docs;
+  booksInfo.forEach((bookInfo) => {
+    const bookCard = document.createElement("div");
+    bookCard.innerHTML = createBookCard(bookInfo);
+    resultArea.appendChild(bookCard);
+  });
 };
 
 const fetchDataFromApi = async (searchText) => {
